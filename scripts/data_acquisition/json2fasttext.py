6305c2a5121ec14cdda9a4121944c1471544c145
TimDeanMoser <tim.moser@uzh.ch>
2020-11-24 16:15:58 +0100

    added sys args to json2fasttext

--- scripts/data_acquisition/json2fasttext.py
+++ scripts/data_acquisition/json2fasttext.py
@@ -1,4 +1,6 @@
 import ast
+import sys
+import os
 
 
 def format_issue(label_name, title, body):
@@ -14,14 +16,33 @@ labels_dict = {
     "Usage Question": "__label__question"
 }
 
-str = open('pandas-dev-pandas.json', 'r', encoding='utf8').read()
-data_set = open('data_set-pandas.txt', 'w', encoding='utf8')
+if __name__ == '__main__':
 
-json_file = ast.literal_eval(str)
-for issue in json_file:
-    if 'labels' not in issue:
-        continue
-    for label in issue['labels']:
-        if 'name' in label:
-            if label['name'] in labels_dict:
-                print(format_issue(label['name'], issue['title'], issue['body']), file=data_set)
+    print('* execute ' + sys.argv[0])
+    # catch missing arguments
+    try:
+        a1 = sys.argv[1]
+        a2 = sys.argv[2]
+    except IndexError as error:
+        print('\033[91m' + "Could not read arguments. Please use the correct command format. Example command:")
+        print("python json2fasttext.py in.json out.txt")
+        exit()
+
+    # get args
+    dump = sys.argv[1]
+    fn_dump = os.path.basename(dump)
+    f_out = sys.argv[2]
+
+    # get files
+    str = open(dump, 'r', encoding='utf8').read()
+    data_set = open(f_out, 'w', encoding='utf8')
+
+    # translate
+    json_file = ast.literal_eval(str)
+    for issue in json_file:
+        if 'labels' not in issue:
+            continue
+        for label in issue['labels']:
+            if 'name' in label:
+                if label['name'] in labels_dict:
+                    print(format_issue(label['name'], issue['title'], issue['body']), file=data_set)
